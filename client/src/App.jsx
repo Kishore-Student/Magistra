32a178bf8a2d6822c4b2c60b1b8ca6bd17c51942
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-21 13:18:42 +0530

    Frontend Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return(
+  <div className="bg-gray-100 min-h-screen">
+    <h1 className="text-2xl font-bold text-center py-4">Welcome to Magistra</h1>
+  </div>
+  );
+}
+
+export default App;

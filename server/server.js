cb2b663a06055b696598498417b4b9a3e4b74c1e
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-22 04:22:30 +0530

    Add server setup with Express, CORS, and routing for assistant responses

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+
+import connectDB from "./config/db.js";
+
+import { generateExplain } from "./explain.js";
+import { generateQuiz } from "./quiz.js";
+import AiRouter from "./routes/qe.routes.js";
+
+
+connectDB();
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+
+
+app.use("/api/assistant-response", AiRouter);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});

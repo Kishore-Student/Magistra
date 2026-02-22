108cc755e8d9479c40a80c776ffcb0748de9054e
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-22 06:58:47 +0530

    fix: correct import path for TeacherDashboard and update route path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,20 @@
 import { Routes , Route} from "react-router-dom";
-import TeacherDashboard from "./pages/Teacher/TDashborad";
+import TeacherDashboard from "./pages/Teacher/TeacherDashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import { Toaster } from "react-hot-toast";
 
 function App() {
+
   return(
+    <>
+      <Toaster />
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/teacher" element={<TeacherDashboard />} />
+        <Route path="/" element={<TeacherDashboard />} />
       </Routes>
+    </>
   );
 }
 

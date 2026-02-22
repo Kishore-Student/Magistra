804fe42cb068b42d8b321bb11fbc73c54ed955ae
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-02-22 06:59:45 +0530

    refactor: simplify generateToken function and add error handling for JWT_SECRET

--- server/config/token.js
+++ server/config/token.js
@@ -1,16 +1,18 @@
-import jwt from 'jsonwebtoken';
-// Function to generate JWT token
-const generateToken = async(userId) => {
-    try{
-        const token = await  jwt.sign(
-            {id: userId}, //payload
-            process.env.JWT_SECRET, //secret key
-            {expiresIn: '10d'} //options
-        );
-        return token;
-    }catch(error){
-        console.log("Error generating token", error);
-    }
-}
+import jwt from "jsonwebtoken";
+
+// Generate JWT token
+const generateToken = (userId) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined");
+  }
+
+  const token = jwt.sign(
+    { id: userId },           // payload
+    process.env.JWT_SECRET,   // secret key
+    { expiresIn: "10d" }      // options
+  );
+
+  return token;
+};
 
 export default generateToken;

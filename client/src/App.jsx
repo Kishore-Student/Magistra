0b37571ce39f06b83e43294c82cac98f4d6490a8
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-03-20 18:13:03 +0530

    feat: wrap TeacherDashboard in ProtectedRoute for access control

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,26 @@
-import { Routes , Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import TeacherDashboard from "./pages/Teacher/TeacherDashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import ProtectedRoute from "./components/ProtectedRoute";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-
-  return(
+  return (
     <>
       <Toaster />
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<TeacherDashboard />} />
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <TeacherDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
